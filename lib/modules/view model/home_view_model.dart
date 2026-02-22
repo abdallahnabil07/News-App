3db3dee3dd26c83ad1c340b515660f891d1319c7
Abdullah Nabil <abdallahnabil770@gmail.com>
2026-02-22 06:53:34 +0200

    -refactor: organize HomeViewModel code and group logic sections✅

--- lib/modules/view model/home_view_model.dart
+++ lib/modules/view model/home_view_model.dart
@@ -6,162 +6,22 @@ import 'package:news/network%20handler/network_handler.dart';
 import '../../model/category_data.dart';
 
 class HomeViewModel extends ChangeNotifier {
+  ////// ================= SEARCH LOGIC ================= //////
+
   bool _isSearching = false;
   String _searchQuery = "";
   List<ArticlesDataModel> _filterArticles = [];
 
-  List<ArticlesDataModel> get articlesShow =>
-      _isSearching ? _filterArticles : _articles;
-  int _selectedTapIndex = 0;
-
-  String _selectCountry = "Egypt";
-
-  ThemeMode _selectTheme = ThemeMode.dark;
-
-  bool isLoading = false;
-
-  String? errorMessage;
-
-  CategoryData? _selectedCategory;
-
-  int page = 1;
-
-  bool isLoadingMore = false;
-
-  bool hasMore = true;
-
-  int get currentIndex => _selectedTapIndex;
-
   bool get isSearching => _isSearching;
-
-  String get selectCountry => _selectCountry;
-
-  ThemeMode get selectTheme => _selectTheme;
-
-  CategoryData? get selectedCategory => _selectedCategory;
-
-  List<SourceData> _sourceData = [];
-
-  List<SourceData> get sourceData => _sourceData;
-
-  List<ArticlesDataModel> _articles = [];
-
-  List<ArticlesDataModel> get articles => _articles;
-
-  List<ArticlesDataModel> get filterArticles => _filterArticles;
 
   String get searchQuery => _searchQuery;
 
-  // get sources from NetworkHandel
-  Future<void> getAllSources(String categoryId) async {
-    try {
-      isLoading = true;
-      errorMessage = null;
-      page = 1;
-      hasMore = true;
-      notifyListeners();
-      _sourceData = await NetworkHandler.getAllSource(categoryId);
-      if (_sourceData.isNotEmpty) {
-        _selectedTapIndex = 0;
-        await getAllArticles();
-      }
-    } catch (error) {
-      errorMessage = ("Something went wrong");
-    } finally {
-      isLoading = false;
-      notifyListeners();
-    }
-  }
+  List<ArticlesDataModel> get filterArticles => _filterArticles;
 
-  // get articles from NetworkHandel
-  Future<List<ArticlesDataModel>> getAllArticles({int page = 1}) async {
-    if (sourceData.isEmpty) return [];
-    try {
-      if (page == 1) {
-        isLoading = true;
-        errorMessage = null;
-        notifyListeners();
-      }
-      final response = await NetworkHandler.getNews(
-        _sourceData[_selectedTapIndex].id,
-        page: page,
-      );
-      if (page == 1) {
-        _articles = response;
-      } else {
-        _articles.addAll(response);
-      }
-      return response;
-    } catch (error) {
-      errorMessage = ("Something went wrong");
-      return [];
-    } finally {
-      if (page == 1) {
-        isLoading = false;
-      }
-      notifyListeners();
-    }
-  }
+  List<ArticlesDataModel> get articlesShow =>
+      _isSearching ? _filterArticles : _articles;
 
-  //refresh Articles
-  Future<void> refresh() async {
-    page = 1;
-    hasMore = true;
-    await getAllArticles(page: 1);
-  }
-
-  // Logic pagination
-  Future<void> loadMore() async {
-    if (isLoadingMore || !hasMore) return;
-    isLoadingMore = true;
-    final newArticle = await getAllArticles(page: page + 1);
-    if (isSearching) {
-      hasMore = false;
-    }
-    if (newArticle.isEmpty) {
-      hasMore = false;
-    } else {
-      page++;
-    }
-    isLoadingMore = false;
-    notifyListeners();
-  }
-
-  //Tap on sources
-  void onTapChaneTapBar(int index) async {
-    _selectedTapIndex = index;
-    page = 1;
-    hasMore = true;
-    notifyListeners();
-    await getAllArticles(page: 1);
-  }
-
-  //drawer(change country)
-  void onTapChaneDrawerCountry(String value) async {
-    _selectCountry = value;
-    notifyListeners();
-  }
-
-  //drawer(change theme)
-  void onTapChaneDrawerTheme(ThemeMode value) async {
-    _selectTheme = value;
-    notifyListeners();
-  }
-
-  //Tap on Category (sport,General,...etc)
-  void onCategoryTap(CategoryData categoryModel) {
-    _selectedCategory = categoryModel;
-    notifyListeners();
-  }
-
-  //drawer back to Category list
-  void goToHome(BuildContext context) {
-    _selectedCategory = null;
-    Navigator.pop(context);
-    notifyListeners();
-  }
-
-  //close search
+  // close search
   void onTapXIconSearching(TextEditingController searchController) {
     _isSearching = false;
     _searchQuery = "";
@@ -170,20 +30,20 @@ class HomeViewModel extends ChangeNotifier {
     notifyListeners();
   }
 
-  //tap on search icon (suffixIcon)
+  // tap search icon
   void onTapIconSearching() {
     _isSearching = !_isSearching;
     notifyListeners();
   }
 
-  //search logic
+  // start search
   void startSearching() {
     _isSearching = true;
     _filterArticles = _articles;
     notifyListeners();
   }
 
-  //
+  // search logic
   void onSearchTextChange(String text) {
     _searchQuery = text;
     if (text.isEmpty) {
@@ -198,6 +58,160 @@ class HomeViewModel extends ChangeNotifier {
           .toList();
       hasMore = _filterArticles.length >= 3;
     }
+    notifyListeners();
+  }
+
+  ////// ================= UI STATE ================= //////
+
+  int _selectedTapIndex = 0;
+  String _selectCountry = "Egypt";
+  ThemeMode _selectTheme = ThemeMode.dark;
+  CategoryData? _selectedCategory;
+
+  int get currentIndex => _selectedTapIndex;
+  String get selectCountry => _selectCountry;
+  ThemeMode get selectTheme => _selectTheme;
+  CategoryData? get selectedCategory => _selectedCategory;
+
+  ////// ================= DATA ================= //////
+
+  List<SourceData> _sourceData = [];
+  List<ArticlesDataModel> _articles = [];
+
+  List<SourceData> get sourceData => _sourceData;
+  List<ArticlesDataModel> get articles => _articles;
+
+  ////// ================= LOADING & PAGINATION ================= //////
+
+  bool isLoading = false;
+  bool isLoadingMore = false;
+  bool hasMore = true;
+
+  String? errorMessage;
+
+  int page = 1;
+
+  ////// ================= NETWORK ================= //////
+
+  // get sources
+  Future<void> getAllSources(String categoryId) async {
+    try {
+      isLoading = true;
+      errorMessage = null;
+      page = 1;
+      hasMore = true;
+      notifyListeners();
+
+      _sourceData = await NetworkHandler.getAllSource(categoryId);
+
+      if (_sourceData.isNotEmpty) {
+        _selectedTapIndex = 0;
+        await getAllArticles();
+      }
+    } catch (error) {
+      errorMessage = ("Something went wrong");
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  // get articles
+  Future<List<ArticlesDataModel>> getAllArticles({int page = 1}) async {
+    if (sourceData.isEmpty) return [];
+
+    try {
+      if (page == 1) {
+        isLoading = true;
+        errorMessage = null;
+        notifyListeners();
+      }
+
+      final response = await NetworkHandler.getNews(
+        _sourceData[_selectedTapIndex].id,
+        page: page,
+      );
+
+      if (page == 1) {
+        _articles = response;
+      } else {
+        _articles.addAll(response);
+      }
+
+      return response;
+    } catch (error) {
+      errorMessage = ("Something went wrong");
+      return [];
+    } finally {
+      if (page == 1) {
+        isLoading = false;
+      }
+      notifyListeners();
+    }
+  }
+
+  ////// ================= PAGINATION ================= //////
+
+  Future<void> refresh() async {
+    page = 1;
+    hasMore = true;
+    await getAllArticles(page: 1);
+  }
+
+  Future<void> loadMore() async {
+    if (isLoadingMore || !hasMore) return;
+
+    isLoadingMore = true;
+
+    final newArticle = await getAllArticles(page: page + 1);
+
+    if (isSearching) {
+      hasMore = false;
+    }
+
+    if (newArticle.isEmpty) {
+      hasMore = false;
+    } else {
+      page++;
+    }
+
+    isLoadingMore = false;
+    notifyListeners();
+  }
+
+  ////// ================= USER ACTIONS ================= //////
+
+  // tap on sources
+  void onTapChaneTapBar(int index) async {
+    _selectedTapIndex = index;
+    page = 1;
+    hasMore = true;
+    notifyListeners();
+    await getAllArticles(page: 1);
+  }
+
+  // change country
+  void onTapChaneDrawerCountry(String value) async {
+    _selectCountry = value;
+    notifyListeners();
+  }
+
+  // change theme
+  void onTapChaneDrawerTheme(ThemeMode value) async {
+    _selectTheme = value;
+    notifyListeners();
+  }
+
+  // category tap
+  void onCategoryTap(CategoryData categoryModel) {
+    _selectedCategory = categoryModel;
+    notifyListeners();
+  }
+
+  // back to home
+  void goToHome(BuildContext context) {
+    _selectedCategory = null;
+    Navigator.pop(context);
     notifyListeners();
   }
 }
